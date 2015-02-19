5903c3a7943c15286af69054c04c58ca2d331b52
Vstoy001 <vicOstoy@gmail.com>
2015-02-18 18:23:49 -0800

    update
    Gave game proper functionality, is near completion

--- project 3/js/app.js
+++ project 3/js/app.js
@@ -5,6 +5,19 @@ var TILE_HEIGHT = 83;
 var CANVAS_WIDTH = 1024;
 var CANVAS_HEIGHT = 960;
 
+var ENEMY_WIDTH = 100;
+var ENEMY_HEIGHT = 70;
+
+var ENEMY_OFFSET = -20;
+var HEART_OFFSET = -30;
+var GEM_OFFSET = -50;
+
+var PLAYER_WIDTH = 70;
+var PLAYER_HEIGHT = 90;
+
+var PLAYER_OFFSET_V = 50;
+var PLAYER_OFFSET_H = 10;
+
 // Enemies our player must avoid
 var Enemy = function(posX, posY, direction, velocity) {
     // Variables applied to each of our instances go here,
@@ -13,8 +26,12 @@ var Enemy = function(posX, posY, direction, velocity) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.spriteN = 'images/enemy-bugN.png';
+    this.spriteW = 'images/enemy-bugW.png';
+    this.spriteS = 'images/enemy-bugS.png';
+    this.spriteE = 'images/enemy-bugE.png';
     this.x = posX;
-    this.y = posY;
+    this.y = posY + ENEMY_OFFSET;
     this.dir = direction;
     this.spd = velocity;
 }
@@ -37,35 +54,104 @@ Enemy.prototype.update = function(dt) {
 }
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    
+Enemy.prototype.render = function(dir) {
+
+    switch(dir)
+    {
+        case "N":
+            ctx.drawImage(Resources.get(this.spriteN), this.x, this.y);
+            break;
+            
+        case "W":
+            ctx.drawImage(Resources.get(this.spriteW), this.x, this.y);
+            break;
+            
+        case "S":
+            ctx.drawImage(Resources.get(this.spriteS), this.x, this.y);
+            break;
+            
+        case "E":
+            ctx.drawImage(Resources.get(this.spriteE), this.x, this.y);
+            break;
+    }
     //console.log("rendered enemy at: " + this.x + " " + this.y);
 }
 
-Enemy.prototype.move = function(pos, dir, spd)
-{
+Enemy.prototype.move = function(pos, dir, spd) {
     if(dir == 'S' || dir == 'E') {
         pos += spd;
         if(pos > CANVAS_WIDTH) {
-            pos = 0 - 20; //wrap enemy around a bit off screen
+            pos = 0 - 100; //wrap enemy around a bit off screen
         }   
     } else {
         pos -= spd;
-        if(pos < 0) {
-            pos = CANVAS_WIDTH + 20; //wrap enemy around a bit off screen
+        if(pos < 0 - ENEMY_WIDTH) {
+            pos = CANVAS_WIDTH + 100; //wrap enemy around a bit off screen
         }
     }
     return pos;
 }
 
+//function that checks for collision
+Enemy.prototype.hitPlayer = function(eX, eY, pX, pY) {
+    pCenterX = pX + PLAYER_WIDTH/2;
+    pCenterY = pY + PLAYER_HEIGHT/2;
+    
+    if( (eX < pX + PLAYER_WIDTH) && (eX + ENEMY_WIDTH > pX) &&
+        (eY < pY + PLAYER_HEIGHT) && (eY + ENEMY_HEIGHT > pY) ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+//heart object, gives player 50 health when picked up
+var PowerUp = function(posX, posY, type, value) {
+    this.x = posX;
+    switch(type) {
+        case "Heart": 
+            this.sprite = 'images/Heart.png';
+            this.y = posY + HEART_OFFSET;
+            break;
+        case "Gem_Blue": 
+            this.sprite = 'images/Gem Blue.png';
+            this.y = posY + GEM_OFFSET;
+            break;
+        case "Gem_Green": 
+            this.sprite = 'images/Gem Green.png';
+            this.y = posY + GEM_OFFSET;
+            break;
+        case "Gem_Orange": 
+            this.sprite = 'images/Gem Orange.png';
+            this.y = posY + GEM_OFFSET;
+            break;
+    }
+    this.val = value;
+}
+
+PowerUp.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+PowerUp.prototype.pickedUp = function(oX, oY, pX, pY) {
+    //for simplicity since hearts are static, we can compare the relative coordinates of the origins 
+    //on the player and heart 
+    if( (Math.abs(oX - pX) < 50) && (Math.abs(oY - pY) < 50) ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(posX, posY) {
+var Player = function(posX, posY, startingHealth, startingScore) {
     this.sprite = 'images/char-boy.png';
-    this.x = posX;
-    this.y = posY;
+    this.health = startingHealth;
+    this.score = startingScore;
+    this.x = posX + PLAYER_OFFSET_H;
+    this.y = posY + PLAYER_OFFSET_V;
     this.comm = "neutral";
 }
 
@@ -94,12 +180,12 @@ Player.prototype.move = function(comm, pos)
 {
     switch(comm) {
         case "left":
-            if(pos/TILE_WIDTH > 0) { 
+            if(pos/TILE_WIDTH > 1) { 
                 pos -= TILE_WIDTH;
             }
             return pos;
         case "up":
-            if(pos/TILE_HEIGHT > 0) { 
+            if(pos/TILE_HEIGHT > 1) { 
                 pos -= TILE_HEIGHT;
             }
             return pos;
@@ -141,18 +227,66 @@ Player.prototype.handleInput = function(dir) {
     }
 }
 
+Player.prototype.reset = function() {
+    //this.sprite = 'images/char-boy.png';
+    this.health = 50;
+    this.score = 0;
+    this.x = 5 * TILE_WIDTH + PLAYER_OFFSET_H;
+    this.y = 9 * TILE_HEIGHT + PLAYER_OFFSET_V;
+    this.comm = "neutral";
+    allPowerUps = [heartA, heartB, heartC, gem_bA, gem_bB, gem_gA, gem_gB, gem_oA, gem_oB];
+}
+
+Player.prototype.checkWin = function() {
+    if(this.y <= TILE_HEIGHT) {
+        return 1;
+    }
+}
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var enemyA = new Enemy(1 * TILE_WIDTH, 2 * TILE_HEIGHT, "E", 10);
-var enemyB = new Enemy(4 * TILE_WIDTH, 5 * TILE_HEIGHT, "S", 5);
-var enemyC = new Enemy(3 * TILE_WIDTH, 6 * TILE_HEIGHT, "E", 10);
+var enemyA = new Enemy(1 * TILE_WIDTH, 2 * TILE_HEIGHT, "E", 8);
+var enemyB = new Enemy(4 * TILE_WIDTH, 5 * TILE_HEIGHT, "E", 6);
+var enemyC = new Enemy(3 * TILE_WIDTH, 6 * TILE_HEIGHT, "W", 4);
+var enemyD = new Enemy(2 * TILE_WIDTH, 2 * TILE_HEIGHT, "E", 4);
+var enemyE = new Enemy(5 * TILE_WIDTH, 3 * TILE_HEIGHT, "W", 6);
+var enemyF = new Enemy(2 * TILE_WIDTH, 2 * TILE_HEIGHT, "E", 4);
+var enemyG = new Enemy(6 * TILE_WIDTH, 3 * TILE_HEIGHT, "W", 8);
 
-var allEnemies = [enemyA, enemyB, enemyC];
+var enemyH = new Enemy(1 * TILE_WIDTH, 6 * TILE_HEIGHT, "E", 6);
+var enemyI = new Enemy(0 * TILE_WIDTH, 7 * TILE_HEIGHT, "E", 2);
+var enemyJ = new Enemy(6 * TILE_WIDTH, 2 * TILE_HEIGHT, "S", 6);
+
+var enemyK = new Enemy(2 * TILE_WIDTH, 4 * TILE_HEIGHT, "S", 8);
+
+var enemyN = new Enemy(8 * TILE_WIDTH, 0 * TILE_HEIGHT, "N", 4);
+var enemyM = new Enemy(5 * TILE_WIDTH, 8 * TILE_HEIGHT, "E", 4);
+
+var enemyO = new Enemy(2 * TILE_WIDTH, 9 * TILE_HEIGHT, "E", 4);
+var enemyP = new Enemy(8 * TILE_WIDTH, 7 * TILE_HEIGHT, "E", 6);
+var enemyQ = new Enemy(7 * TILE_WIDTH, 8 * TILE_HEIGHT, "E", 6);
+
+
+
+var allEnemies = [enemyA, enemyB, enemyC, enemyD, enemyE, enemyF, enemyG, enemyH, enemyI, enemyJ, enemyK, enemyN, enemyM, enemyO, enemyP, enemyQ];
+
+var heartA = new PowerUp(9 * TILE_WIDTH, 5 * TILE_HEIGHT, "Heart", 0);
+var heartB = new PowerUp(1 * TILE_WIDTH, 4 * TILE_HEIGHT, "Heart", 0);
+var heartC = new PowerUp(4 * TILE_WIDTH, 6 * TILE_HEIGHT, "Heart", 0);
+
+var gem_bA = new PowerUp(1 * TILE_WIDTH, 5 * TILE_HEIGHT, "Gem_Blue", 100);
+var gem_bB = new PowerUp(8 * TILE_WIDTH, 5 * TILE_HEIGHT, "Gem_Blue", 100);
+var gem_gA = new PowerUp(1 * TILE_WIDTH, 8 * TILE_HEIGHT, "Gem_Green", 50);
+var gem_gB = new PowerUp(8 * TILE_WIDTH, 8 * TILE_HEIGHT, "Gem_Green", 50);
+var gem_oA = new PowerUp(1 * TILE_WIDTH, 2 * TILE_HEIGHT, "Gem_Orange", 150);
+var gem_oB = new PowerUp(8 * TILE_WIDTH, 2 * TILE_HEIGHT, "Gem_Orange", 150);
+
+var allPowerUps = [heartA, heartB, heartC, gem_bA, gem_bB, gem_gA, gem_gB, gem_oA, gem_oB];
 
 //create a player at coordinate (x, y)
-var player = new Player(5 * TILE_WIDTH, 5 * TILE_HEIGHT);
+var player = new Player(5 * TILE_WIDTH, 9 * TILE_HEIGHT, 50, 0);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- project 3/js/engine.js
+++ project 3/js/engine.js
@@ -17,7 +17,9 @@
 //constants for dimensions of a tile
 var BLOCK_WIDTH = 83;
 var BLOCK_HEIGHT = 101;
-var CHAR_OFFSET = 20;
+
+var CANVAS_WIDTH = 1024;
+var CANVAS_HEIGHT = 960;
 
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
@@ -30,8 +32,8 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 1024; //505;
-    canvas.height = 960; //606;
+    canvas.width = CANVAS_WIDTH; //505;
+    canvas.height = CANVAS_HEIGHT; //606;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -47,6 +49,7 @@ var Engine = (function(global) {
         var now = Date.now(),
             dt = (now - lastTime) / 1000.0;
 
+        //console.log("Your dt has been set to: " + dt);
         /* Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
@@ -85,7 +88,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
          updateEntities(dt);
-        // checkCollisions();
+         checkCollisions();
     }
 
     /* This is called by the update function  and loops through all of the
@@ -110,7 +113,35 @@ var Engine = (function(global) {
         
     }
     
-    
+    function checkCollisions() {
+        allEnemies.forEach(
+            function(enemy) {
+                //drain health during contact
+                if( enemy.hitPlayer(enemy.x, enemy.y, player.x, player.y) ) {
+                    player.health-=2;
+                } 
+                //check if player has won or has lost all health to reset
+                if( player.checkWin() || player.health <= 0) {
+                    reset();
+                }
+            }
+        );
+        
+        allPowerUps.forEach(
+            function(power) {
+                if( power.pickedUp(power.x, power.y, player.x, player.y) ) {
+                    //gain health on contact
+                    if(power.val == 0) {
+                        player.health += 50;
+                    }
+                    player.score += power.val;
+                    var index = allPowerUps.indexOf(power);
+                    allPowerUps.splice(index, 1);
+                } 
+            }
+        );
+        
+    }
 
     /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
@@ -123,14 +154,14 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var gameWorld = 1,
-        gameRow1 = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-        gameRow2 = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-        gameRow3 = [ 1, 1, 1, 1, 1, 0, 0, 0, 2, 2 ],
-        gameRow4 = [ 2, 2, 0, 0, 0, 0, 0, 0, 2, 2 ],
-        gameRow5 = [ 2, 2, 0, 0, 0, 0, 0, 0, 2, 2 ],
-        gameRow6 = [ 2, 2, 2, 2, 2, 0, 1, 1, 1, 1 ],
-        gameRow7 = [ 2, 2, 2, 2, 2, 0, 1, 1, 1, 1 ],
-        gameRow8 = [ 2, 2, 2, 2, 2, 0, 1, 1, 1, 1 ],
+        gameRow1 = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        gameRow2 = [ 1, 1, 1, 1, 0, 0, 2, 2, 2, 2 ],
+        gameRow3 = [ 1, 1, 1, 1, 0, 0, 2, 2, 2, 2 ],
+        gameRow4 = [ 2, 2, 1, 1, 1, 1, 1, 1, 2, 2 ],
+        gameRow5 = [ 2, 2, 1, 1, 1, 1, 1, 1, 2, 2 ],
+        gameRow6 = [ 2, 2, 2, 2, 2, 1, 1, 1, 1, 1 ],
+        gameRow7 = [ 2, 2, 2, 2, 2, 1, 1, 1, 1, 1 ],
+        gameRow8 = [ 2, 2, 2, 2, 2, 1, 1, 1, 1, 1 ],
         gameRow9 = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
         gameRowA = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
         gameGrid = [
@@ -149,22 +180,41 @@ var Engine = (function(global) {
          * and, using the rowImages array, draw the correct image for that
          * portion of the "grid"
          */
+        
+        //clear screen completely for redraw
+        ctx.fillStyle = "white";
+        ctx.fillRect(-50, -50, CANVAS_WIDTH + 50, CANVAS_HEIGHT + 50);
+        
         for (row = 0; row < numRows; row++) {
             for (col = 0; col < numCols; col++) {
                 /* The drawImage function of the canvas' context element
                  * requires 3 parameters: the image to draw, the x coordinate
                  * to start drawing and the y coordinate to start drawing.
                  * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
+                 * so thast we get the benefits of caching these images, since
                  * we're using them over and over.
                  */
                 //index of imageKey indicates the block to place based off the index on the gameGrid made up of gameRows
-                ctx.drawImage(Resources.get(imageKey[ gameGrid[row][col] ]), col * BLOCK_HEIGHT, row * BLOCK_WIDTH + CHAR_OFFSET);
+                ctx.drawImage(Resources.get(imageKey[ gameGrid[row][col] ]), col * BLOCK_HEIGHT, row * BLOCK_WIDTH);
             }
         }
-
-
+        
+        overlayText();
+        
         renderEntities();
+    }
+    
+    //function for setting up scoreboard information
+    function overlayText() {
+        ctx.strokeRect(0,0, CANVAS_WIDTH - 15, 50);
+        
+        ctx.fillStyle = "#ccc";
+        ctx.font = "32pt Verdana";
+        ctx.fillText("health: " + player.health, 5, 40);
+        ctx.lineWidth = 2;
+        ctx.strokeText("health: " + player.health, 5, 40);
+        ctx.fillText("score: " + player.score, 705, 40);
+        ctx.strokeText("score: " + player.score, 705, 40);
     }
 
     /* This function is called by the render function and is called on each game
@@ -174,12 +224,20 @@ var Engine = (function(global) {
     function renderEntities() {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
-         */   
-        allEnemies.forEach(
-            function(enemy) {
-                enemy.render();
+         */  
+        
+        allPowerUps.forEach(
+            function(power) {
+                power.render();
             }
         );
+        
+        allEnemies.forEach(
+            function(enemy) {
+                enemy.render(enemy.dir);
+            }
+        );
+        
         
         player.render();
     }
@@ -190,6 +248,12 @@ var Engine = (function(global) {
      */
     function reset() {
         // noop
+        player.reset();
+        /*
+        ctx.fillStyle = "white";
+        ctx.fillRect(200, 200, CANVAS_WIDTH - 200, CANVAS_HEIGHT - 200);
+        ctx.strokeRect(200, 200, CANVAS_WIDTH - 200, CANVAS_HEIGHT - 200);
+        */
     }
 
     /* Go ahead and load all of the images we know we're going to need to
@@ -200,8 +264,16 @@ var Engine = (function(global) {
         'images/stone-block.png',
         'images/water-block.png',
         'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png'
+        //'images/enemy-bug.png',
+        'images/enemy-bugN.png',
+        'images/enemy-bugS.png',
+        'images/enemy-bugE.png',
+        'images/enemy-bugW.png',
+        'images/char-boy.png',
+        'images/Heart.png',
+        'images/Gem Blue.png',
+        'images/Gem Green.png',
+        'images/Gem Orange.png'
     ]);
     Resources.onReady(init);
 
